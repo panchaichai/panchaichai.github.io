d7ba873598ea458495204ea461587db7e443591d
panchaichai <pjz0306@163.com>
2014-11-25 21:20:43 +0800

    forest-2-1

--- forest-3-1/project/js/js.js
+++ forest-3-1/project/js/js.js
@@ -3,13 +3,13 @@ var number=5; //定义条目数
 function LMYC() {
 var lbmc;
     for (i=1;i<=number;i++) {
-        lbmc = eval('LM' + i);
+        lbmc = eval('LM' + i)[0];
         lbmc.style.display = 'none';
     }
 }
  
 function ShowFLT(i) {
-    lbmc = eval('LM' + i);
+    lbmc = eval('LM' + i)[0];
     if (lbmc.style.display == 'none') {
         LMYC();
         lbmc.style.display = '';
